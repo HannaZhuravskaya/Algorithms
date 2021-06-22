63a96b3e8ec43f3220a3851fd80cc111f5ad8e6c
Hanna Zhuravskaya <hanna.zhuravskaya@gmail.com>
2021-06-22 11:21:56 +0300

    added 792 optimal

--- Algorithms/LeetCode/792.NumberOfMatchingSubsequences.cs
+++ Algorithms/LeetCode/792.NumberOfMatchingSubsequences.cs
@@ -1,7 +1,45 @@
+using System.Collections.Generic;
+
 namespace Algorithms.LeetCode
 {
     public class NumberOfMatchingSubsequencesTask
     {
+        public int NumMatchingSubseq(string s, string[] words)
+        {
+            var dict = new Dictionary<char, Queue<string>>();
 
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                dict[i] = new Queue<string>();
+            }
+
+            for (int i = 0; i < words.Length; ++i)
+            {
+                dict[words[i][0]].Enqueue(words[i]);
+            }
+
+            var cnt = 0;
+
+            foreach (var letter in s)
+            {
+                if (!dict.ContainsKey(letter))
+                    continue;
+
+                var queue = dict[letter];
+                var size = queue.Count;
+
+                for (int i = 0; i < size; ++i)
+                {
+                    var w = queue.Dequeue();
+
+                    if (w.Length == 1)
+                        cnt++;
+                    else
+                        dict[w[1]].Enqueue(w.Substring(1));
+                }
+            }
+
+            return cnt;
+        }
     }
 }
